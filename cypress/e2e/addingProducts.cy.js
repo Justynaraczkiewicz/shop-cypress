43a70916cb8b9f62a002efb1c6cd77d211db6220
Justyna Raczkiewicz <jraczkiewicz@software.altkom.pl>
2024-06-27 15:45:03 +0200

    add logout test

--- cypress/e2e/addingProducts.cy.js
+++ cypress/e2e/addingProducts.cy.js
@@ -2,22 +2,23 @@ import { products } from "../support/constants";
 import { enterToDetails } from "../support/shared";
 
 describe("Adding products to card", () => {
-  const getAmountInCart = cy.get('span[class*="shopping_cart_badge"]');
+ 
 
   beforeEach("loginToApplication", () => {
     cy.loginToApplication();
   });
 
   it("Adding Product to Cart from Details", () => {
+
     for (const product of products) {
       enterToDetails(product);
       cy.get("button").contains("ADD TO CART").click();
-      getAmountInCart.should("contain", "1");
+      getCartBadge().should("contain", "1");
       // Wejście do koszyka
       cy.get('[id="shopping_cart_container"]').click();
       //Usunięcie z koszyka
       cy.get('button[class*="cart_button"]').click();
-      getAmountInCart.should("not.exist");
+      getCartBadge().should("not.exist");
       //Kliknięcie Continue Shopping
       cy.get('[class="btn_secondary"]')
         .should("contain", "Continue Shopping")
@@ -25,4 +26,8 @@ describe("Adding products to card", () => {
     }
   });
 });
+
+function getCartBadge() {
+  return cy.get('span[class*="shopping_cart_badge"]');
+}
 
